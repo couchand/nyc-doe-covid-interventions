24294afcc03b38ee4934285b3d09edd679fa3904
Andrew Dona-Couch <hi@andrewcou.ch>
2023-01-12 00:18:13 -0500

    allow script to run on same repo

--- copy-all.sh
+++ copy-all.sh
@@ -5,6 +5,10 @@ set -x
 
 PREV=$(git branch --show-current)
 
+cp copy-commit-date.sh copy-commit-date-gitproof.sh
+
 git checkout main
-git log --grep='^School cases:' --pretty='%H %cs' | xargs -IXXX ./copy-commit-date.sh XXX
+git log --grep='^School cases:' --pretty='%H %cs' | xargs -IXXX ./copy-commit-date-gitproof.sh XXX
 git checkout $PREV
+
+rm copy-commit-date-gitproof.sh
